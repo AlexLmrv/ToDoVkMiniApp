000c0587ff73fc52f21f956e0e361bf0cff8f962
AlexLmrv <AlexLmrv@users.noreply.github.com>
2019-11-29 17:58:26 +0700

    Updates

---
+++ precache-manifest.878e887b553b6011d17d29a5703fc2d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9558ca7e8862e7a2051808477c1bb14e",
+    "url": "/ToDoVkMiniApp/index.html"
+  },
+  {
+    "revision": "1bc05068816ae1cdb4fd",
+    "url": "/ToDoVkMiniApp/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "dfb6d5b69ca869327a9c",
+    "url": "/ToDoVkMiniApp/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "1bc05068816ae1cdb4fd",
+    "url": "/ToDoVkMiniApp/static/js/2.555ade03.chunk.js"
+  },
+  {
+    "revision": "dfb6d5b69ca869327a9c",
+    "url": "/ToDoVkMiniApp/static/js/main.712928c6.chunk.js"
+  },
+  {
+    "revision": "7ce30d5222f5ab620045",
+    "url": "/ToDoVkMiniApp/static/js/runtime-main.d1c3b975.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/ToDoVkMiniApp/static/media/persik.4e1ec840.png"
+  }
+]);

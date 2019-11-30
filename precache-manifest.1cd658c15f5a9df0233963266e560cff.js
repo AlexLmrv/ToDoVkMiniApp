56b0ac7d56a3e8c53082104288cfd5cecfae85d4
Alexey <Lymarev>
2019-11-30 14:50:57 +0700

    Updates

---
+++ precache-manifest.1cd658c15f5a9df0233963266e560cff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502c97cac935e9cc2c98d462434c5bd1",
+    "url": "/ToDoVkMiniApp/index.html"
+  },
+  {
+    "revision": "9b35b3ced923c728c8f8",
+    "url": "/ToDoVkMiniApp/static/css/2.2cfa1b23.chunk.css"
+  },
+  {
+    "revision": "c050bdf733adf48b8953",
+    "url": "/ToDoVkMiniApp/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "9b35b3ced923c728c8f8",
+    "url": "/ToDoVkMiniApp/static/js/2.b86e08d8.chunk.js"
+  },
+  {
+    "revision": "c050bdf733adf48b8953",
+    "url": "/ToDoVkMiniApp/static/js/main.44f2162c.chunk.js"
+  },
+  {
+    "revision": "487ee976cdfb743036c9",
+    "url": "/ToDoVkMiniApp/static/js/runtime-main.646664c8.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/ToDoVkMiniApp/static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ToDoVkMiniApp/precache-manifest.0ae932c9ff548a6896d05de329f01901.js"
+  "/ToDoVkMiniApp/precache-manifest.1cd658c15f5a9df0233963266e560cff.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ToDoVkMiniApp/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
